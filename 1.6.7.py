3b5fb62085f3ff192e200d4049ee8849a8d28f8a
beemaster1972 <ugor.finno@yandex.ru>
2023-02-06 16:48:35 +0500

    1.6.7

--- 1.6.7.py
+++ 1.6.7.py
@@ -1,2 +1,16 @@
 class SingletonFive:
-    pass
+
+    __instance = []
+
+    def __new__(cls, *args, **kwargs):
+        if len(cls.__instance) < 5:
+            cls.__instance.append(super().__new__(cls))
+        return cls.__instance[-1]
+
+    def __init__(self, name):
+        self.name = name
+
+
+objs = [SingletonFive(str(n)) for n in range(10)]
+for obj in objs:
+    print (obj.name)
