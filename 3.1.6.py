a18c3be0f79a74768920214ee711220274b57eee
beemaster1972 <ugor.finno@yandex.ru>
2023-04-25 14:02:17 +0500

    3.1.6

--- 3.1.6.py
+++ 3.1.6.py
@@ -37,7 +37,7 @@ class Mummies(Exhibit):
         return exh
 
 
-class Papyri:
+class Papyri(Exhibit):
     def __new__(cls, *args, **kwargs):
         exh = super().__new__(cls)
         exh.check_attr = Exhibit.check_attr
@@ -47,8 +47,59 @@ class Papyri:
 
 
 class Museum:
-    pass
+
+    def __init__(self, name: str):
+        self.name = name
+        self.exhibits = []
+
+    def add_exhibit(self, obj):
+        self.exhibits.append(obj)
+
+    def remove_exhibit(self, obj):
+        self.exhibits.remove(obj)
+
+    def get_info_exhibit(self, indx):
+        if indx < len(self.exhibits):
+            name = self.exhibits[indx].name
+            descr = self.exhibits[indx].descr
+        else:
+            name = 'Не известно'
+            descr = 'Не известно'
+        return f"Описание экспоната {name}: {descr}"
 
 
-pic = Picture('9 val', 'Aivazovsky', ' Aivazovsky was paint 9 val')
-print(pic.name, pic.author, pic.descr)
+# TEST-TASK___________________________________
+mus = Museum("Эрмитаж")
+assert type(mus.name) is str, "название должно быть строкой"
+assert mus.exhibits == [], "exhibits должен быть списком"
+assert hasattr(mus, "add_exhibit"), "метод не объявлен"
+assert hasattr(mus, "remove_exhibit"), "метод не объявлен"
+assert hasattr(mus, "get_info_exhibit"), "метод не объявлен"
+
+pic = Picture("Балакирев с подписчиками пишет письмо иноземному султану", "Неизвестный автор",
+              "Вдохновляющая, устрашающая, волнующая картина")
+assert 'name' in pic.__dict__.keys() and 'descr' in pic.__dict__.keys() and 'author' in pic.__dict__.keys(), "ошибка в локальных атрибутах"
+
+mum = Mummies("Балакирев", "Древняя Россия", "Просветитель XXI века, удостоенный мумификации")
+assert 'name' in mum.__dict__.keys() and 'location' in mum.__dict__.keys() and 'descr' in mum.__dict__.keys(), "ошибка в локальных атрибутах"
+
+p = Papyri("Ученья для, не злата ради",
+           "Древняя Россия",
+           "Самое древнее найденное рукописное свидетельство о языках программирования")
+assert 'name' in p.__dict__.keys(), "ошибка в локальном атрибуте name"
+assert 'date' in p.__dict__.keys(), "ошибка в локальном атрибуте date"
+assert 'descr' in p.__dict__.keys(), "ошибка в локальном атрибуте descr"
+assert type(p.date) is str, "название должно быть строкой"
+
+
+mus.add_exhibit(pic)
+assert mus.exhibits[0] == pic and len(mus.exhibits) == 1, "некорректно отработал метод add_exhibit"
+
+mus.remove_exhibit(pic)
+assert len(mus.exhibits) == 0 and pic not in mus.exhibits, "некорректно отработал метод remove_exhibit"
+
+mus.add_exhibit(p)
+mus.add_exhibit(pic)
+answ = mus.get_info_exhibit(0)
+assert answ == f"Описание экспоната {mus.exhibits[0].name}: {mus.exhibits[0].descr}", "некорректно отработал метод get_info_exhibit"
+print("Правильный ответ.")
