3b9536221d4e5a7b5886bc97d55f0d4aed138ce5
beemaster1972 <ugor.finno@yandex.ru>
2023-05-04 15:59:47 +0500

    3.3.5

---
+++ 3.3.5.py
@@ -0,0 +1,104 @@
+class Value:
+
+    def __set_name__(self, owner, name):
+        self.name = '__' + name
+
+    def __get__(self, instance, owner):
+        return instance.__dict__[self.name]
+
+    def __set__(self, instance, value):
+        instance.__dict__[self.name] = value
+
+
+class ObjList:
+
+    __data = Value()
+    __next = Value()
+    __prev = Value()
+
+    def __init__(self, data: str):
+        self.__data = data
+        self.__prev = None
+        self.__next = None
+
+
+class LinkedList:
+    len_list = 0
+
+    def __init__(self):
+        self.head = None
+        self.tail = None
+
+    def add_obj(self, obj):
+        if not self.tail:
+            self.head = obj
+            self.tail = obj
+        elif self.head is self.tail:
+            self.head.next = obj
+            obj.prev = self.head
+            self.tail = obj
+        else:
+            self.tail.next = obj
+            obj.prev = self.tail
+            self.tail = obj
+
+        self.len_list += 1
+
+    def remove_obj(self, indx):
+        cur_obj = self.head
+        i = 0
+        while i < self.len_list:
+            if i == indx:
+                try:
+                    cur_obj.prev.next = cur_obj.next
+                    cur_obj.next.prev = cur_obj.prev
+                except:
+                    self.head = cur_obj.next
+                    cur_obj.next.prev = None
+                self.len_list -= 1
+                break
+            else:
+                i += 1
+                cur_obj = cur_obj.next
+
+    def __call__(self, *args, **kwargs):
+        cur_obj = self.head
+        i = 0
+        while i < self.len_list:
+            if i == args[0]:
+                return cur_obj.data
+            i += 1
+            cur_obj = cur_obj.next
+
+    def __len__(self):
+        return self.len_list
+
+
+ln = LinkedList()
+ln.add_obj(ObjList("Сергей"))
+ln.add_obj(ObjList("Балакирев"))
+ln.add_obj(ObjList("Python ООП"))
+ln.remove_obj(2)
+assert len(ln) == 2, "функция len вернула неверное число объектов в списке, возможно, неверно работает метод remove_obj()"
+ln.add_obj(ObjList("Python"))
+assert ln(2) == "Python", "неверное значение атрибута __data, взятое по индексу"
+assert len(ln) == 3, "функция len вернула неверное число объектов в списке"
+assert ln(1) == "Балакирев", "неверное значение атрибута __data, взятое по индексу"
+
+n = 0
+h = ln.head
+while h:
+    assert isinstance(h, ObjList)
+    h = h._ObjList__next
+    n += 1
+
+assert n == 3, "при перемещении по списку через __next не все объекты перебрались"
+
+n = 0
+h = ln.tail
+while h:
+    assert isinstance(h, ObjList)
+    h = h._ObjList__prev
+    n += 1
+
+assert n == 3, "при перемещении по списку через __prev не все объекты перебрались"
