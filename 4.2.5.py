5bc42aeb89c6f1e4cb2943e621ce391ad66f3b59
beemaster1972 <ugor.finno@yandex.ru>
2023-06-24 13:18:08 +0500

    4.2.5

--- 4.2.5.py
+++ 4.2.5.py
@@ -1,7 +1,9 @@
 class Protists:
 
-    def __init__(self, string: str):
-        pass
+    def __init__(self, name: str, weight: float, old: int):
+        self.name = name
+        self.weight = weight
+        self.old = old
 
 
 class Plants(Protists):
@@ -50,3 +52,15 @@ class Flower(Flowering):
 
 class Worm(Worms):
     pass
+
+
+if __name__ == '__main__':
+    objects_string = ('Monkey, "мартышка", 30.4, 7',
+                      'Monkey, "шимпанзе", 24.6, 8',
+                      'Person, "Балакирев", 88, 34',
+                      'Person, "Верховный жрец", 67.5, 45',
+                      'Flower, "Тюльпан", 0.2, 1',
+                      'Flower, "Роза", 0.1, 2',
+                      'Worm, "червь", 0.01, 1',
+                      'Worm, "червь 2", 0.02, 1')
+    
